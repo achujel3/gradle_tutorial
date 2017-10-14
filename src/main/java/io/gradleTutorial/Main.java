8d1f5b1955d01e0cdc193a5ec41be124f44cf4af
roba <achujel3@gmail.com>
2017-10-14 16:27:16 +0300

    First commit. Simple Gradle test

---
+++ src/main/java/io/gradleTutorial/Main.java
@@ -0,0 +1,11 @@
+package main.java.io.gradleTutorial;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+
+}
